e6354bb103d7a52fb164bc154274587e8f495318
AishwaryaMurali <aishumurali98@gmail.com>
2020-06-02 15:36:39 +0530

    Initial commit

---
+++ app/src/main/java/sample/example/com/jitsiconf/MainActivity.java
@@ -0,0 +1,47 @@
+package sample.example.com.jitsiconf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        URL serverUrl;
+        try{
+            serverUrl = new URL("https://meet.jit.si");
+        }catch (MalformedURLException ex){
+            ex.printStackTrace();
+            throw new RuntimeException("Invalid server URL!");
+        }
+        JitsiMeetConferenceOptions defaultOptions = new JitsiMeetConferenceOptions.Builder().setServerURL(serverUrl).setWelcomePageEnabled(false).build();
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+    }
+
+    public void onButtonClick(View v){
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+        if (text.length() > 0) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder().setRoom(text).build();
+
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(this, options);
+        }
+    }
+}
